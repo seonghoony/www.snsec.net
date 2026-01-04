697077a61749c12435942a16c3e3e3234c8a8779
Seonghoon Jeong <seonghoon@shjeong.net>
2026-01-04 19:20:41 +0900

    revised people tab

--- _scripts/search.liquid.js
+++ _scripts/search.liquid.js
@@ -79,7 +79,7 @@ ninja.data = [
     {%- endfor -%}
   {%- endif -%}
   {%- for collection in site.collections -%}
-    {%- if collection.label != 'posts' -%}
+    {%- if collection.label != 'posts' and collection.label != 'templates' and collection.label != 'template_projects' -%}
       {%- for item in collection.docs -%}
         {
           {%- if item.inline -%}
