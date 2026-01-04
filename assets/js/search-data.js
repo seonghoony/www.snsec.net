6d725f87025785a70e0d8c4b0094fe707f7c1ef8
seonghoony <seonghoon@shjeong.net>
2026-01-04 10:48:42 +0000

    Deploying to gh-pages from @ seonghoony/www.snsec.net@53592a7f1ae6ae20a602c3d9015caf283f6a8080 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -228,16 +228,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-course-reviews-from-students-are-now-available-at-course-reviews",
+          title: 'Course reviews from students are now available at Course Reviews.',
           description: "",
           section: "News",},{
         id: 'social-cv',
